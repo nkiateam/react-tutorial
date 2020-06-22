5cd66a1b3733b9e53c6869d000545047ebbdb95d
sypark96 <sypark96@nkia.co.kr>
2020-06-23 08:55:28 +0900

    error 소스 커밋

--- examples/08-error/app.js
+++ examples/08-error/app.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import Error from './components/Error';
+import App from './src/App';
+
 
 class Main extends Component {
     render() {
         return (
-            <div><h1>숫자가 4이상이면 에러발생</h1><Error/></div>
+            <App />
             
         );
     }
---
+++ examples/08-error/src/components/Error.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const Promblematic = () => {
+    throw (new Error('버그 발생...!'));  // Error가 발생하면 자동으로 componentDidCatch 로 이동
+};
+
+class Error extends React.Component {
+    state = {
+        number: 0,
+        error: false,
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({
+            error: true,
+        });
+    }
+
+    handleIncrese = () => {
+        const { number } = this.state;
+        this.setState({
+            number: number + 1,
+        });
+    }
+
+    handleDecrease = () => {
+        this.setState(({ number }) => ({
+            number: number - 1,
+        }));
+    }
+
+    handleReset = () => {
+        this.setState({
+            number: 0,
+            error: false,
+        });
+    }
+
+    render() {
+        const { error, number } = this.state;
+        if (error) return (
+            <React.Fragment>
+                <h1>에러 발생...!</h1>
+                <button onClick={this.handleReset}>Reset</button>
+            </React.Fragment>
+        )
+
+        return (
+            <div>
+                <div>값: {number}</div>
+                { number === 4 && <Promblematic /> }
+                <button onClick={this.handleIncrese}>+</button>
+                <button onClick={this.handleDecrease}>-</button>
+            </div>
+        )
+    }
+}
+
+export default Error;
