81b10194aa544571c355601c918ac4b5c539181f
sypark96 <sypark96@nkia.co.kr>
2020-06-29 16:29:34 +0900

    form 예제 수정

--- examples/06-form/src/App.js
+++ examples/06-form/src/App.js
@@ -7,16 +7,19 @@ class App extends React.Component {
     this.state = {
       value: '',
     }
+
+    this.input = React.createRef();
   }
 
-  handleChange = (event) => {
-    this.setState({
-      value: event.target.value
-    });
-  }
+  // handleChange = (event) => {
+  //   this.setState({
+  //     value: event.target.value
+  //   });
+  // }
 
   handleSubmit = (event) => {
-    alert('나의 이름은 ' + this.state.value);
+    // alert('나의 이름은 ' + this.state.value); // 제어 컴포넌트에서 데이터 가져오기
+    alert('나의 이름은 ' + this.input.current.value); // 비제어 컴포넌트에서 데이터 가져오기
     event.preventDefault();  // 페이지 리로딩 방지
   }
 
@@ -25,7 +28,12 @@ class App extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           Name: 
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input
+            type="text"
+            // value={this.state.value}
+            // onChange={this.handleChange}
+            ref={this.input}
+          />
         </label>
         <input type="submit" vlaue="Submit" />
       </form>
